10775426e9e6fef1e6d71ca647b925c5454fad2c
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2022-02-01 21:11:59 +0100

    created NoContent component

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,3 +1,21 @@
+import { Box } from '@mui/material';
+import NoContentImage from '../../assets/no_content.png';
+import { ImageWithInfo } from './ImageWithInfo';
+
 export const NoContent = () => {
-  return null;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <ImageWithInfo
+        imageText="Brak danych do wyświetlenia"
+        imageSource={NoContentImage}
+      />
+    </Box>
+  );
 };
